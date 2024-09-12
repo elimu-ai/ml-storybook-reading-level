ae3109c46f069327ad923175845fe9606cdac5a2
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-09-12 20:43:55 +0700

    feat: accuracy score
    closes #20

--- pmml/step3_predict/step3_2_validate.py
+++ pmml/step3_predict/step3_2_validate.py
@@ -1,25 +1,25 @@
-import os
+from os.path import basename
 import pandas
 from pypmml import Model
 from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import accuracy_score
 
 # Load Predictive Model Markup Language (PMML) model
 model_pmml_path = '../step2_train/step2_2_model.pmml'
-print(os.path.basename(__file__), 'model_pmml_path: {}'.format(model_pmml_path))
+print(basename(__file__), f'model_pmml_path: {model_pmml_path}')
 reading_level_model = Model.load(model_pmml_path)
-print(os.path.basename(__file__), 'reading_level_model: {}'.format(reading_level_model))
+print(basename(__file__), f'reading_level_model: {reading_level_model}')
 
 # Read the preprocessed test data CSV into a DataFrame
 storybooks_csv_path = '../step1_prepare/step1_3_storybooks_test.csv'
-print(os.path.basename(__file__), 'storybooks_csv_path: {}'.format(storybooks_csv_path))
 storybooks_dataframe = pandas.read_csv(storybooks_csv_path)
-print(os.path.basename(__file__), 'storybooks_dataframe:  \n{}'.format(storybooks_dataframe))
+print(basename(__file__), f'storybooks_dataframe:  \n{storybooks_dataframe}')
 
 # Predict
 storybook_features = ['chapter_count', 'paragraph_count', 'word_count']
 predictions = reading_level_model.predict(storybooks_dataframe[storybook_features])
-print(os.path.basename(__file__), 'predictions:  \n{}'.format(predictions))
-print(os.path.basename(__file__), 'type(predictions):  \n{}'.format(type(predictions)))
+print(basename(__file__), f'predictions:  \n{predictions}')
+print(basename(__file__), f'type(predictions):  \n{type(predictions)}')
 
 # Write the DataFrame to a CSV file
 predictions.to_csv('step3_2_predictions.csv', index=False)
@@ -28,8 +28,16 @@ predictions.to_csv('step3_2_predictions.csv', index=False)
 val_y = storybooks_dataframe['reading_level']
 val_predictions = predictions
 mean_absolute_error = mean_absolute_error(val_y, val_predictions)
-print(os.path.basename(__file__), 'mean_absolute_error: {}'.format(mean_absolute_error))
+print(basename(__file__), f'mean_absolute_error: {mean_absolute_error}')
 
 # Write the Mean Absolute Error (MAE) to a text file
 with open('step3_2_mean_absolute_error.txt', 'w') as file:
     file.write(str(mean_absolute_error))
+
+# Calculate the accuracy score
+accuracy_score = accuracy_score(val_y, val_predictions)
+print(basename(__file__), f'accuracy_score: {accuracy_score}')
+
+# Write the accuracy score to a text file
+with open('step3_2_accuracy_score.txt', 'w') as file:
+    file.write(str(accuracy_score))
