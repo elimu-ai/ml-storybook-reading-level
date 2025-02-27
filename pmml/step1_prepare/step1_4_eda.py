e4aa6d4d36efe13745a17fb699895a03ef163a78
Sarthak Shah <sarthakshah0204@gmail.com>
2025-02-27 10:20:30 +0100

    Made scatter-plots with regression line

--- pmml/step1_prepare/step1_4_eda.py
+++ pmml/step1_prepare/step1_4_eda.py
@@ -1,6 +1,8 @@
 from os.path import basename
-from matplotlib import pyplot
 import pandas
+import seaborn as sns
+import matplotlib.pyplot as pyplot
+
 
 # Read the preprocessed storybooks CSV into a DataFrame
 storybooks_csv_path = 'step1_2_storybooks.csv'
@@ -10,11 +12,18 @@ storybooks_dataframe = pandas.read_csv(storybooks_csv_path)
 for label in storybooks_dataframe.columns[2:]:
     print(f'label: {label}')
 
-    # Scatter
-    pyplot.scatter(storybooks_dataframe['reading_level'], storybooks_dataframe[label], alpha=0.5)
+    # Scatter plot with regression line and confidence interval
+    pyplot.figure(figsize=(8, 6))
+    sns.regplot(x=storybooks_dataframe['reading_level'], 
+                y=storybooks_dataframe[label], 
+                scatter_kws={'alpha': 0.5},  # Transparency for scatter points
+                line_kws={'color': 'red'})  # Regression line color
+    
+    # Labels and saving
     pyplot.xlabel('reading_level')
     pyplot.ylabel(label)
-    pyplot.savefig(f'step1_4_{label}_scatter.png')
+    pyplot.title(f'Scatter Plot with Regression for {label}')
+    pyplot.savefig(f'step1_4_{label}_scatter_with_regression.png')
     pyplot.clf()
 
     # Histogram
