d5737a5d2ba2eab03bca67803aee5208968e5af4
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-04-16 09:41:06 +0700

    refactor: webapp-lfs
    #59

--- pmml/step1_prepare/step1_1_download_data.py
+++ pmml/step1_prepare/step1_1_download_data.py
@@ -2,8 +2,7 @@ import os
 import pandas
 
 # Read the storybooks CSV into a DataFrame
-storybooks_csv_url = ('https://raw.githubusercontent.com/elimu-ai/webapp/main/src/main/resources/db/content_PROD'
-                          '/hin/storybooks.csv')
+storybooks_csv_url = ('https://raw.githubusercontent.com/elimu-ai/webapp-lfs/main/lang-HIN/storybooks.csv')
 print(os.path.basename(__file__), 'storybooks_csv_url: {}'.format(storybooks_csv_url))
 storybooks_dataframe = pandas.read_csv(storybooks_csv_url)
 print(os.path.basename(__file__), 'storybooks_dataframe:\n{}'.format(storybooks_dataframe))
